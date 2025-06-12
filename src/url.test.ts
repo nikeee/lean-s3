515eaa25e4604f041a4324760322f152f7a5d58e
Niklas Mollenhauer <nikeee@outlook.com>
2025-06-12 11:11:06 +0200

    Migrate url

---
+++ src/url.test.ts
@@ -0,0 +1,130 @@
+import { describe, test } from "node:test";
+import { expect } from "expect";
+
+import { buildRequestUrl } from "./url.js";
+
+describe("buildRequestUrl", () => {
+	test("aws", () => {
+		expect(
+			buildRequestUrl(
+				"https://{bucket}.s3.{region}.amazonaws.com",
+				"mybucket",
+				"us-west-1",
+				"object.json",
+			),
+		).toStrictEqual(
+			new URL("https://mybucket.s3.us-west-1.amazonaws.com/object.json"),
+		);
+
+		expect(
+			buildRequestUrl(
+				"https://{bucket}.s3.{region}.amazonaws.com",
+				"mybucket",
+				"us-west-1",
+				"/object.json",
+			),
+		).toStrictEqual(
+			new URL("https://mybucket.s3.us-west-1.amazonaws.com/object.json"),
+		);
+
+		expect(
+			buildRequestUrl(
+				"https://{bucket}.s3.us-west-1.amazonaws.com",
+				"mybucket",
+				"us-west-1",
+				"/object.json",
+			),
+		).toStrictEqual(
+			new URL("https://mybucket.s3.us-west-1.amazonaws.com/object.json"),
+		);
+
+		expect(
+			buildRequestUrl(
+				"https://{bucket}.s3.{region}.amazonaws.com/",
+				"/mybucket/",
+				"us-west-1",
+				"/object.json",
+			),
+		).toStrictEqual(
+			new URL("https://mybucket.s3.us-west-1.amazonaws.com/object.json"),
+		);
+
+		expect(
+			buildRequestUrl(
+				"https://{bucket}.s3.{region}.amazonaws.com/",
+				"/mybucket/",
+				"eu-west-1",
+				"object.json",
+			),
+		).toStrictEqual(
+			new URL("https://mybucket.s3.eu-west-1.amazonaws.com/object.json"),
+		);
+	});
+
+	test("r2", () => {
+		expect(
+			buildRequestUrl(
+				"https://my-account-id.r2.cloudflarestorage.com",
+				"/mybucket/",
+				"auto",
+				"object.json",
+			),
+		).toStrictEqual(
+			new URL(
+				"https://my-account-id.r2.cloudflarestorage.com/mybucket/object.json",
+			),
+		);
+
+		expect(
+			buildRequestUrl(
+				"https://my-account-id.r2.cloudflarestorage.com/",
+				"mybucket",
+				"auto",
+				"object.json",
+			),
+		).toStrictEqual(
+			new URL(
+				"https://my-account-id.r2.cloudflarestorage.com/mybucket/object.json",
+			),
+		);
+
+		expect(
+			buildRequestUrl(
+				"https://my-account-id.eu.r2.cloudflarestorage.com/",
+				"mybucket",
+				"auto",
+				"object.json",
+			),
+		).toStrictEqual(
+			new URL(
+				"https://my-account-id.eu.r2.cloudflarestorage.com/mybucket/object.json",
+			),
+		);
+
+		expect(
+			buildRequestUrl(
+				"https://{bucket}.my-account-id.r2.cloudflarestorage.com",
+				"mybucket",
+				"auto",
+				"object.json",
+			),
+		).toStrictEqual(
+			new URL(
+				"https://mybucket.my-account-id.r2.cloudflarestorage.com/object.json",
+			),
+		);
+	});
+
+	test("hetzner", () => {
+		expect(
+			buildRequestUrl(
+				"https://fsn1.your-objectstorage.com",
+				"mybucket",
+				"auto",
+				"object.json",
+			),
+		).toStrictEqual(
+			new URL("https://fsn1.your-objectstorage.com/mybucket/object.json"),
+		);
+	});
+});
---
+++ src/url.ts
@@ -0,0 +1,79 @@
+export function buildRequestUrl(
+	endpoint: string,
+	bucket: string,
+	region: string,
+	path: string,
+): URL {
+	const normalizedBucket = normalizePath(bucket);
+
+	const [endpointWithBucketAndRegion, replacedBucket] =
+		replaceDomainPlaceholders(endpoint, normalizedBucket, region);
+
+	const result = new URL(endpointWithBucketAndRegion);
+
+	const pathPrefix = result.pathname.endsWith("/")
+		? result.pathname
+		: `${result.pathname}/`;
+
+	const pathSuffix = replacedBucket
+		? normalizePath(path)
+		: `${normalizedBucket}/${normalizePath(path)}`;
+
+	result.pathname = pathPrefix + pathSuffix;
+
+	return result;
+}
+
+function replaceDomainPlaceholders(
+	endpoint: string,
+	bucket: string,
+	region: string,
+): [endpoint: string, replacedBucket: boolean] {
+	const replacedBucket = endpoint.includes("{bucket}");
+	return [
+		endpoint.replaceAll("{bucket}", bucket).replaceAll("{region}", region),
+		replacedBucket,
+	];
+}
+
+/**
+ * Removes trailing and leading slash.
+ */
+function normalizePath(path: string): string {
+	const start = path[0] === "/" ? 1 : 0;
+	const end = path[path.length - 1] === "/" ? path.length - 1 : path.length;
+	return path.substring(start, end);
+}
+
+/**
+ * Sorts headers alphabetically. Removes headers that are undefined/null.
+ *
+ * `http.request` doesn't allow passing `undefined` as header values (despite the types allowing it),
+ * so we have to filter afterwards.
+ */
+export function prepareHeadersForSigning(
+	unfilteredHeadersUnsorted: Record<string, string | undefined>,
+): Record<string, string> {
+	const result: Record<string, string> = {};
+
+	// TODO: `Object.keys(headersUnsorted).sort()` is constant in our case,
+	// maybe we want to move this somewhere else to avoid sorting every time
+
+	for (const header of Object.keys(unfilteredHeadersUnsorted).sort()) {
+		const v = unfilteredHeadersUnsorted[header];
+		if (v !== undefined && v !== null) {
+			result[header] = v;
+		}
+	}
+	return result;
+}
+
+export function getRangeHeader(
+	start: number | undefined,
+	endExclusive: number | undefined,
+): string | undefined {
+	return typeof start === "number" || typeof endExclusive === "number"
+		? // Http-ranges are end-inclusive, we are exclusiv ein our slice
+			`bytes=${start ?? 0}-${typeof endExclusive === "number" ? endExclusive - 1 : ""}`
+		: undefined;
+}

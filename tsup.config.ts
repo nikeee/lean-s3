1777a82df73f7d83317202ecdc58655aa9fd038d
Niklas Mollenhauer <nikeee@outlook.com>
2025-06-15 02:06:48 +0200

    Format

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'tsup'
+import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts"],
-  format: ["esm"],
-  target: "esnext",
-  dts: true,
-  clean: true,
-})
+	entry: ["src/index.ts"],
+	format: ["esm"],
+	target: "esnext",
+	dts: true,
+	clean: true,
+});

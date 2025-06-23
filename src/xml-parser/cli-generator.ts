1c06eed6b53c1d2f31d7e414b3bc90560b18b4a7
Niklas Mollenhauer <nikeee@outlook.com>
2025-06-23 17:17:07 +0200

    Add usage note

--- src/xml-parser/cli-generator.ts
+++ src/xml-parser/cli-generator.ts
@@ -1,3 +1,11 @@
+/**
+ * @module Used to generate a static file containing a parser module for debugging purposes.
+ * Invoke using:
+ * ```sh
+ * tsx ./cli-generator.ts > parse.js
+ * ```
+ */
+
 import { buildStaticParserSource } from "./generator.ts";
 
 const source = buildStaticParserSource({

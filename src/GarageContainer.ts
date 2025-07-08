7677393e3be482681d77e0ee68667ea45e04fcc3
Niklas Mollenhauer <nikeee@outlook.com>
2025-07-08 16:07:04 +0200

    Create keys on startup

--- src/GarageContainer.ts
+++ src/GarageContainer.ts
@@ -2,33 +2,95 @@ import {
 	AbstractStartedContainer,
 	GenericContainer,
 	Wait,
+	type StartedTestContainer,
 } from "testcontainers";
 
 const S3_PORT = 9000;
+const ADMIN_API_PORT = 3903;
 
 export class GarageContainer extends GenericContainer {
-	username = "garageadmin";
-	password = "garageadmin";
+	adminToken = "test-admin-token-for-ci";
 
 	constructor() {
 		super("ghcr.io/nikeee/lean-s3-ci-images/garage:latest");
 		this.withExposedPorts(S3_PORT);
-		this.withWaitStrategy(Wait.forLogMessage("S3 API server listening on"));
+		this.withExposedPorts(ADMIN_API_PORT);
+
+		this.withWaitStrategy(
+			Wait.forAll([
+				Wait.forLogMessage("Admin API server listening on"),
+				Wait.forLogMessage("S3 API server listening on"),
+			]),
+		);
 	}
 
 	override async start(): Promise<StartedGarageContainer> {
 		const startedContainer = await super.start();
-		return new StartedGarageContainer(startedContainer);
+		const credentials = await GarageContainer.#createAccessKey(
+			startedContainer,
+			this.adminToken,
+		);
+		// TODO: Set up initial bucket and credentials
+		return new StartedGarageContainer(
+			startedContainer,
+			credentials.accessKeyId,
+			credentials.secretAccessKey,
+		);
+	}
+
+	static async #createAccessKey(
+		container: StartedTestContainer,
+		adminToken: string,
+	) {
+		const apiBase = `http://${container.getHost()}:${container.getMappedPort(ADMIN_API_PORT)}`;
+		const res = await fetch(`${apiBase}/v2/CreateKey`, {
+			method: "POST",
+			body: JSON.stringify({
+				allow: null,
+				deny: null,
+				name: "testcontainer-access",
+				neverExpires: true,
+				expiration: null,
+			}),
+			headers: {
+				Authorization: `Bearer ${adminToken}`,
+				"Content-Type": "application/json",
+			},
+		});
+
+		// biome-ignore lint/suspicious/noExplicitAny: shrug
+		const json = (await res.json()) as any;
+		console.log("garage response", json);
+		return {
+			accessKeyId: json.accessKeyId,
+			secretAccessKey: json.secretAccessKey,
+		};
 	}
 }
 
 export class StartedGarageContainer extends AbstractStartedContainer {
+	accessKeyId: string;
+	secretAccessKey: string;
+
+	constructor(
+		startedContainer: StartedTestContainer,
+		accessKeyId: string,
+		secretAccessKey: string,
+	) {
+		super(startedContainer);
+		this.accessKeyId = accessKeyId;
+		this.secretAccessKey = secretAccessKey;
+	}
+
 	getPort() {
 		return this.startedTestContainer.getMappedPort(S3_PORT);
 	}
-
-	getAccessKeyId(): string {}
-	getSecretAccessKey(): string {}
+	getAccessKeyId(): string {
+		return this.accessKeyId;
+	}
+	getSecretAccessKey(): string {
+		return this.secretAccessKey;
+	}
 
 	getConnectionUrl() {
 		return `http://${this.getHost()}:${this.getPort()}`;

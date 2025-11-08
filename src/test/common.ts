45fbad3c994db883223739789e832e5dfc9e9719
Niklas Mollenhauer <nikeee@outlook.com>
2025-11-08 01:00:05 +0100

    Fixes landed in rustfs/s3mock
    See: https://github.com/rustfs/rustfs/issues/762
    
    See: https://github.com/adobe/S3Mock/pull/2731

--- src/test/common.ts
+++ src/test/common.ts
@@ -332,15 +332,6 @@ export function runTests(
 			}
 		});
 		test("n too large", async t => {
-			if (implementation === "rustfs" || implementation === "s3mock") {
-				// Refs:
-				// https://github.com/rustfs/rustfs/issues/762
-				//  https://github.com/adobe/S3Mock/pull/2731
-				t.todo(
-					`S3 implementation "${implementation}" does not implement this correctly; upstream fix underway`,
-				);
-			}
-
 			const testId = crypto.randomUUID();
 			const path = `${runId}/${testId}/slicing.txt`;
 			const f = client.file(path);

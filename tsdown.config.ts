0aab98ae54b712ae14251efa180e2b4de8f181f0
Niklas Mollenhauer <nikeee@outlook.com>
2025-11-14 23:20:44 +0100

    Add wasm plugin

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from "tsdown";
+import zig from "vite-plugin-zig-wasm";
 
 export default defineConfig({
 	entry: ["src/index.ts"],
 	target: "esnext",
+	plugins: [
+		zig(),
+	],
 });

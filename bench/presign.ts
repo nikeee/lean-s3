f82b1a0f321dc9174bfacea4ddcc1f79ebf05626
Niklas Mollenhauer <nikeee@outlook.com>
2025-11-24 00:51:25 +0100

    Update lockfile

--- bench/presign.ts
+++ bench/presign.ts
@@ -90,13 +90,6 @@ mitata.summary(async () => {
 			secretKey: "sample-secret-key",
 		});
 
-		const policy =client.newPostPolicy();
-		policy.setExpires(new Date(Date.now() + 3600 * 1000));
-		policy.setKey("path.json");
-		policy.setBucket("test-bucket");
-		policy.setContentLengthRange(10, 1000);
-
-		await client.presignedPostPolicy(policy)
 		mitata
 			.bench("minio client", async () => {
 				await client.presignedGetObject("test-bucket", "path.json");

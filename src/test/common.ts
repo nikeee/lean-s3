896b6189c5241e224e3a096d5da3d8e6b11a8315
Niklas Mollenhauer <nikeee@outlook.com>
2025-12-04 15:35:17 +0100

    Fix in s3mock released

--- src/test/common.ts
+++ src/test/common.ts
@@ -912,13 +912,6 @@ export function runTests(
 			expect(res1.contents.length).toBe(0);
 		});
 		test("with strings", async t => {
-			if (implementation === "s3mock") {
-				// Ref: See https://github.com/adobe/S3Mock/issues/2755
-				t.todo(
-					`S3 implementation "${implementation}" does not implement this feature`,
-				);
-			}
-
 			const testId = crypto.randomUUID();
 			await client
 				.file(`${runId}/${testId}/test-a-0.txt`)

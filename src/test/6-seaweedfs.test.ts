15e5d1d74b33f307b6d0675a097234d213960b6b
Niklas Mollenhauer <nikeee@outlook.com>
2025-12-09 17:48:44 +0100

    Add seaweed to test matrix

---
+++ src/test/6-seaweedfs.test.ts
@@ -0,0 +1,42 @@
+import { after, before, describe } from "node:test";
+import { expect } from "expect";
+
+import { runTests } from "./common.ts";
+import { S3Client } from "../index.ts";
+import { SeaweedFSContainer } from "./SeaweedFSContainer.ts";
+
+describe("seaweedfs", async () => {
+	const s3 = await new SeaweedFSContainer().start();
+	const region = "us-east-1";
+	const bucket = "test-bucket-seaweedfs";
+
+	const runId = Date.now();
+	{
+		const client = new S3Client({
+			endpoint: s3.getConnectionUrl(),
+			accessKeyId: s3.getAccessKeyId(),
+			secretAccessKey: s3.getSecretAccessKey(),
+			region,
+			bucket: "none", // intentionally set to a non-existent one, so we catch cases where the bucket is not passed correctly
+		});
+		before(async () => {
+			const res = await client.createBucket(bucket);
+			expect(res).toBeUndefined();
+		});
+		after(async () => {
+			expect(await client.bucketExists(bucket)).toBe(true);
+			await client.deleteBucket(bucket);
+			expect(await client.bucketExists(bucket)).toBe(false);
+			await s3.stop();
+		});
+	}
+	runTests(
+		runId,
+		s3.getConnectionUrl(),
+		s3.getAccessKeyId(),
+		s3.getSecretAccessKey(),
+		region,
+		bucket,
+		"seaweedfs",
+	);
+});

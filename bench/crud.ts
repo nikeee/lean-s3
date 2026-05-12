27b31103f3be5101c627ac9997e30e312d533b05
Niklas Mollenhauer <nikeee@outlook.com>
2026-05-12 05:31:27 +0200

    Fix linter error

--- bench/crud.ts
+++ bench/crud.ts
@@ -154,12 +154,10 @@ const clients: ClientWrapper[] = [
 					Key: key,
 				}),
 			);
-			// oxlint-disable-next-line typescript-eslint(no-non-null-asserted-optional-chain): :shrug:
-			return await res.Body?.transformToByteArray()!;
+			return await res.Body?.transformToByteArray();
 		},
 		list: async (bucket, prefix) => {
 			const r = await awsS3.send(new ListObjectsV2Command({ Bucket: bucket, Prefix: prefix }));
-			// oxlint-disable-next-line typescript-eslint(no-non-null-asserted-optional-chain): :shrug:
 			return r.KeyCount!;
 		},
 		delete: async (bucket, key) => {
